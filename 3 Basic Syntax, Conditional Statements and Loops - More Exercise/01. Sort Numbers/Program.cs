9a5e26243a3c97e3ff34a13fa54223154837f23e
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-04-10 08:44:46 +0300

    Exercises

---
+++ 3 Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/Program.cs
@@ -0,0 +1,74 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int maxNumer = int.MinValue;
+int middleNumber = 0;
+int minNumber = int.MaxValue;
+int countA = 0, countB = 0, countC = 0;
+
+for (int i = 1; i <= 3; i++)
+{
+    if (countA == 0 && a > maxNumer)
+    {
+        countA++;
+        maxNumer = a;
+    }
+    else if (countB == 0 && b > maxNumer)
+    {
+        countB++;
+        maxNumer = b;
+    }
+    else if (countC == 0 && c > maxNumer)
+    {
+        countC++;
+        maxNumer = c;
+    }
+}
+countA = 0; countB = 0; countC = 0;
+
+for (int i = 1; i <= 3; i++)
+{
+    if (countA == 0 && a < minNumber)
+    {
+        countA++;
+        minNumber = a;
+    }
+    else if (countB == 0 && b < minNumber)
+    {
+        countB++;
+        minNumber = b;
+    }
+    else if (countC == 0 && c < minNumber)
+    {
+        countC++;
+        minNumber = c;
+    }
+}
+
+if (a < maxNumer && a > minNumber)
+{
+    middleNumber = a;
+}
+else if (b < maxNumer && b > minNumber)
+{
+    middleNumber = b;
+}
+else if (c < maxNumer && c > minNumber)
+{
+    middleNumber = c;
+}
+else if (a == minNumber)
+{
+    middleNumber = a;
+}
+else if (b == minNumber)
+{
+    middleNumber = b;
+}
+else if (c == minNumber)
+{
+    middleNumber = c;
+}
+Console.WriteLine(maxNumer);
+Console.WriteLine(middleNumber);
+Console.WriteLine(minNumber);
---
+++ 3 Basic Syntax, Conditional Statements and Loops - More Exercise/02. English Name of the Last Digit/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+
+string s = Console.ReadLine();
+char letter = ' ';
+for (int i = 0; i < s.Length; i++)
+{
+    letter = s[i];
+    if (i < s.Length-1)
+    {}
+    else
+    {
+        if (letter == '1') 
+        {
+            Console.WriteLine("one");
+        }
+        else if (letter == '2')
+        {
+            Console.WriteLine("two");
+        }
+        else if (letter == '3')
+        {
+            Console.WriteLine("three");
+        }
+        else if (letter == '4')
+        {
+            Console.WriteLine("four");
+        }
+        else if (letter == '5')
+        {
+            Console.WriteLine("five");
+        }
+        else if (letter == '6')
+        {
+            Console.WriteLine("six");
+        }
+        else if (letter == '7')
+        {
+            Console.WriteLine("seventh");
+        }
+        else if (letter == '8')
+        {
+            Console.WriteLine("eight");
+        }
+        else if (letter == '9')
+        {
+            Console.WriteLine("nine");
+        }
+
+    }
+}
+
