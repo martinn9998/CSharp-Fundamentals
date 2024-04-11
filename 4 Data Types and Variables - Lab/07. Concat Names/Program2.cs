acb92088849aee0e370eb73db8c82689ee672d9b
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-04-11 11:13:59 +0300

    Update Program2.cs

---
+++ 4 Data Types and Variables - Lab/07. Concat Names/Program2.cs
@@ -0,0 +1,13 @@
+namespace ConcatNames
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            string str3 = Console.ReadLine();
+            Console.WriteLine(str1 + str3 + str2);
+        }
+    }
+}
