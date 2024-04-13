b58eda141c8ff30751540995b9e7ffe64a50e5af
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-04-13 17:18:05 +0300

    Update Program2.cs

---
+++ 6 Data Types and Variables - More Exercise/06. Balanced Brackets/Program2.cs
@@ -0,0 +1,37 @@
+namespace  BalancedBrackets
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            int linesNumber = int.Parse(Console.ReadLine());
+            int openedBracketsCount = 0;
+            int closedBracketsCount = 0;
+            for (int i = 0; i < linesNumber; i++)
+            {
+                string input = Console.ReadLine();
+                if (input == "(")
+                {
+                    openedBracketsCount++;
+                }
+                else if (input == ")")
+                {
+                    closedBracketsCount++;
+                    if (openedBracketsCount - closedBracketsCount != 0) 
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+            if (openedBracketsCount ==  closedBracketsCount)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
