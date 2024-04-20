c0030410d363c7d5a0863047dce4d06670f4066b
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-04-20 13:06:18 +0300

    Summary

--- 10 Methods - Lab/01. Sign of Integer Numbers Third Solution/obj/Debug/net6.0/01. Sign of Integer Numbers Third Solution.AssemblyInfo.cs
+++ 10 Methods - Lab/01. Sign of Integer Numbers Third Solution/obj/Debug/net6.0/01. Sign of Integer Numbers Third Solution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. Sign of Integer Numbers Third Solution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a167b3f46fac262245b8483eaf5b96ad05a64dab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9c539dc4f8db58eb655072a25b0a64706addab")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. Sign of Integer Numbers Third Solution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. Sign of Integer Numbers Third Solution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 10 Methods - Lab/07. Repeat String Second Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _07._Repeat_String_Second_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int repeatingsCount = int.Parse(Console.ReadLine());
+        }
+        static string RepeatString(string str, int repeatingCount)
+    }
+}
