8c8e7edb0c5a3384a7b08d74568338ecf2481bbe
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-04-21 21:41:28 +0300

    Summary

---
+++ 11 Methods - Exercise/01. Smallest of Three Numbers Second Solution/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01._Smallest_of_Three_Numbers_Second_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSmallestNumber(firstNumber, secondNumber
+                , thirdNumber));
+        }
+        static int GetSmallestNumber(int firstNumber, int secondNumber, 
+            int thirdNumber)
+        {
+            int smallestNumber = Math.Min(Math.Min(firstNumber, secondNumber), 
+                thirdNumber);
+            return smallestNumber;
+        }
+    }
+}
--- 11 Methods - Exercise/01. Smallest of Three Numbers/obj/Debug/net6.0/01. Smallest of Three Numbers.AssemblyInfo.cs
+++ 11 Methods - Exercise/01. Smallest of Three Numbers/obj/Debug/net6.0/01. Smallest of Three Numbers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. Smallest of Three Numbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b6c9e9bfd62c570a2504b00d4a2d74c3a45af4")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. Smallest of Three Numbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. Smallest of Three Numbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
