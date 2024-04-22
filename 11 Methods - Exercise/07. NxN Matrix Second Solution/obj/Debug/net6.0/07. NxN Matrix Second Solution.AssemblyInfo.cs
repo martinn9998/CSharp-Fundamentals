23eb82ee3cff8c118292a0d7c5ee389c4836248f
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-04-22 21:14:24 +0300

    Summary

--- 11 Methods - Exercise/07. NxN Matrix Second Solution/obj/Debug/net6.0/07. NxN Matrix Second Solution.AssemblyInfo.cs
+++ 11 Methods - Exercise/07. NxN Matrix Second Solution/obj/Debug/net6.0/07. NxN Matrix Second Solution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("07. NxN Matrix Second Solution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64bb1fc3eb3d7e59b23edf053076a54bd5287699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17979203a442bbc16d4fd779378435b7d5d0e596")]
 [assembly: System.Reflection.AssemblyProductAttribute("07. NxN Matrix Second Solution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("07. NxN Matrix Second Solution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
