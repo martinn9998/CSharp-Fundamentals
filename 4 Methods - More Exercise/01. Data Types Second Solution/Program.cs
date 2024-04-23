6f8cf5d77fd0e47902698f60f06313d12e3fd3f3
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-04-23 19:56:35 +0300

    Summary

---
+++ 4 Methods - More Exercise/01. Data Types Second Solution/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _01._Data_Types_Second_Solution
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "int":
+                    Console.WriteLine(IsInt());
+                    break;
+                case "real":
+                    Console.WriteLine($"{isReal():f2}");
+                    break;
+                case "string":
+                    Console.WriteLine(isString());
+                    break;
+            }
+        }
+        static int IsInt()
+        {
+            int number = int.Parse(Console.ReadLine());
+            number *= 2;
+            return number;
+        }
+        static double isReal()
+        {
+            double number = Math.Abs(double.Parse(Console.ReadLine()));
+            number *= 1.5;
+            return number;
+        }
+        static string isString()
+        {
+            string word = Console.ReadLine();
+            string finalWord = "$" + word + "$";
+            return finalWord;
+        }
+    }
+}
