8fcfa04eee2500fa370a3fc21c7a536c3010aaba
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-05-01 18:20:47 +0300

    Summary

--- 23 Text Processing - Exercise/08. Letters Change Numbers/Program.cs
+++ 23 Text Processing - Exercise/08. Letters Change Numbers/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> str = Console
+                .ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<double> sums = new List<double>();
+            for (int i = 0; i < str.Count; i++)
+            {
+                double currSum = 0;
+                string currStringNumber = "";
+                int numbersCount = 1;
+                for (int j = 0; j < str[i].Length; j++)
+                {
+                    if (str[i][j] >= 48 && str[i][j] <= )
+                }
+            }
         }
     }
 }
