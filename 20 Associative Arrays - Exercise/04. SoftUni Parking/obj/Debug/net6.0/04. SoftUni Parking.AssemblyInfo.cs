47a4f99754de4faee03bbd87d994ff2ec2016e25
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-05-01 12:16:18 +0300

    Summary

--- 20 Associative Arrays - Exercise/04. SoftUni Parking/obj/Debug/net6.0/04. SoftUni Parking.AssemblyInfo.cs
+++ 20 Associative Arrays - Exercise/04. SoftUni Parking/obj/Debug/net6.0/04. SoftUni Parking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04. SoftUni Parking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c198a12398a8fa7be90a9670d5c312273a122124")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede997325d2a40f284fd9f1256284a0db9cc16b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("04. SoftUni Parking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04. SoftUni Parking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 20 Associative Arrays - Exercise/05. Courses/Program.cs
+++ 20 Associative Arrays - Exercise/05. Courses/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var dict = new Dictionary<string, List<string>>();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                List<string> courseArgs = input
+                    .Split(" : ")
+                    .ToList();
+                string courseName = courseArgs[0];
+                string studentNmae = courseArgs[1];
+                if (!dict.ContainsKey(courseName))
+                {
+                    dict[courseName] = new List<string>();
+                }
+                dict[courseName] = new List<string>();
+            }
+            foreach (var item in dict.OrderByDescending(X => X.Value.Count()))
+            {
+                string courseName = item.Key;
+                int studsCount = item.Value.Count();
+                Console.WriteLine($"{courseName}: {studsCount}");
+                foreach (var studs in item.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {studs}");
+                }
+            }
         }
     }
 }
