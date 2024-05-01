7d40739608a76a12a743d661e919755c3d710bcb
martinn9998 <martichbalsha@gmail.com>
2024-05-01 13:36:14 +0300

    Summary

--- 22 Text Processing - Lab/01. Reverse Strings/Program.cs
+++ 22 Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace _01._Reverse_Strings
+﻿using System.Text;
+
+namespace _01._Reverse_Strings
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string word;
+            var sb = new StringBuilder();
+            var sbTwo = new StringBuilder();
+            while ((word = Console.ReadLine()) != "end")
+            {
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    sbTwo.Append(word[i]);
+                }
+                sb.AppendLine($"{word} = {sbTwo}");
+                sbTwo.Clear();
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
