f03b53360afb2630369ee3fa67287bd92a4fe2fd
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-05-27 22:20:31 +0300

    Add

---
+++ Exams/01. Programming Fundamentals Mid Exam Retake/01. Computer Store/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+ 1050
+   200
+   450
+   2
+   18.50 
+   16.86 
+   special
+   
+ */
+
+namespace _01._Computer_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string client;
+            decimal sum = 0m;
+            decimal taxes = 0m;
+            while ((client = Console.ReadLine()) != "special")
+            {
+                if (client == "regular")
+                {
+                    break;
+                }
+                decimal price = decimal.Parse(client);
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    continue;
+                }
+                    sum = sum + price;
+                    taxes += price * 0.2m;
+            }
+            decimal totalPrice = sum + taxes;
+            if (client == "special")
+            {
+                totalPrice *= 0.9m;
+            }
+
+            if (totalPrice == 0)
+            {
+                Console.WriteLine("Invalid order!");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {sum:f2}$");
+                Console.WriteLine($"Taxes: {taxes:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {totalPrice:f2}$");
+            }
+        }
+    }
+}
---
+++ Exams/01. Programming Fundamentals Mid Exam Retake/02. The Lift/Program.cs
@@ -0,0 +1,73 @@
+﻿
+namespace _02._The_Lift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int[] liftCurrentState = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int j = 0;
+            bool isItTrue = false;
+            for (int i = 0; i < liftCurrentState.Length; i++)
+            {
+                if (people >= 4)
+                {
+                    for (; j < liftCurrentState.Length;)
+                    {
+                        if (liftCurrentState[j] > 0)
+                        {
+                            int currentNumberForArray = liftCurrentState[j];
+                            people -= 4 - currentNumberForArray;
+                            liftCurrentState[j] += (4 - currentNumberForArray);
+                        }
+                        else if (liftCurrentState[j] == 0)
+                        {
+                            liftCurrentState[j] += 4;
+                            people -= 4;
+                        }
+                            j++;
+                        
+                        break;
+                    }
+                    continue;
+                }
+                else if (people < 4 && people > 0)
+                {
+                    for (; j < liftCurrentState.Length; )
+                    {
+                        liftCurrentState[j] += people;
+                        people -= liftCurrentState[j];
+                        i = liftCurrentState.Length;
+                        break;
+                    }
+                    continue;
+                }
+            }
+
+            if (people > 0)
+            {
+                isItTrue = true;
+            }
+            else if (people <= 0)
+            {
+                isItTrue = false;
+            }
+
+            if (isItTrue)
+            {
+                Console.WriteLine($"There isn't enough space! {people} people in a queue!");
+                Console.WriteLine(string.Join(" ", liftCurrentState));
+            }
+            else
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", liftCurrentState));
+            }
+        }
+    }
+}
