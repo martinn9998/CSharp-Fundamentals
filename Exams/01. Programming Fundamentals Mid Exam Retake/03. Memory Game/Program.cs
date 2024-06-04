80178d0ed43cb041406656919c49024e415a5378
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-06-04 16:02:42 +0300

    04.06.24 Addition

--- Exams/01. Programming Fundamentals Mid Exam Retake/03. Memory Game/Program.cs
+++ Exams/01. Programming Fundamentals Mid Exam Retake/03. Memory Game/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace _03._Memory_Game
+﻿/*
+ 
+
+1 1 2 2 3 3 4 4 5 5 
+1 0
+ -1 0
+1 0 
+1 0 
+1 0 
+end
+   
+
+
+
+
+ */
+
+
+namespace _03._Memory_Game
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<int> sequenceOfElements = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+
+            }
         }
     }
 }
