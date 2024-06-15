58611c045a4e6142553b0d3efe1dc90e28c17e4b
martinn9998 <martichbalsha@gmail.com>
2024-06-15 23:17:04 +0300

    Add

--- Exams/02. Programming Fundamentals Mid Exam/02. Array Modifier/Program.cs
+++ Exams/02. Programming Fundamentals Mid Exam/02. Array Modifier/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main()
         {
-            int[] arr = Console
+            int[] arr = Console 
                 .ReadLine()
                 .Split()
                 .Select(int.Parse)
@@ -16,8 +16,20 @@
                 string process = inputArr[0];
                 if (process == "swap")
                 {
-
+                    int number1 = arr[int.Parse(inputArr[1])];
+                    int number2 = arr[int.Parse(inputArr[2])];
+                    arr[int.Parse(inputArr[1])] = number2;
+                    arr[int.Parse(inputArr[2])] = number1;
                 }
+                else if (process == "multiply")
+                {
+                     
+                }
+                else if (process == "decrease" &&)
+                {
+                     
+                }
+                else if (process >= )
             }
         }
     }
