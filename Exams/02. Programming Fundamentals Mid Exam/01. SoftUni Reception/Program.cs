267b38e94ab5406657056a4a6a44c91c83016428
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-06-19 17:19:14 +0300

    Add

--- Exams/02. Programming Fundamentals Mid Exam/01. SoftUni Reception/Program.cs
+++ Exams/02. Programming Fundamentals Mid Exam/01. SoftUni Reception/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace SoftUniReception
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] eployeeEfficiency = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int studentCount = int.Parse(Console.ReadLine());
+        }
+    }
+}
