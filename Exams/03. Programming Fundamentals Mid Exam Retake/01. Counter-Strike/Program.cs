da1187d349f699c5c237fe6880fc98173acac74e
Martin Tanev <166533842+martinn9998@users.noreply.github.com>
2024-06-21 11:36:35 +0300

    Add

---
+++ Exams/03. Programming Fundamentals Mid Exam Retake/01. Counter-Strike/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Counter_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Exams/03. Programming Fundamentals Mid Exam Retake/02. Shoot for the Win/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._Shoot_for_the_Win
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
